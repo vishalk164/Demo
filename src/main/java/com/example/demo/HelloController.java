24c06adf2d34430b00ba35e4d7299ad95530f50e
Vishal S Kamble <vishalkamble375@gmail.com>
2025-02-18 09:24:15 +0530

    new update

--- src/main/java/com/example/demo/HelloController.java
+++ src/main/java/com/example/demo/HelloController.java
@@ -5,22 +5,17 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-@RequestMapping("/")
 public class HelloController {
 
-    @GetMapping
+    @GetMapping("/new")
     public String home() {
         return "Application is running successfull";
+        
     }
     
-    @GetMapping("new")
-    public String home2() {
-    	return "Application is runnning on another path";
-    }
-    
-    @GetMapping("new1")
-    public String home3() {
-    	return "Application is runnning on another Path 2";
-    	
+    @GetMapping("/new/new")
+    public String home1() {
+        return "Application is running successfull";
+        
     }
 }
